dc6c271364bd88015af7e590b3fee542a96f808b
Violeta Saravia <saraviavioleta@gmail.com>
2024-12-02 01:43:57 +0000

    .

--- day01.sh
+++ day01.sh
@@ -1,5 +1,12 @@
 powershell.exe Get-Clipboard | awk '{print $1}' | sort > new.txt
 powershell.exe Get-Clipboard | awk '{print $2}' | sort > new2.txt
 paste -d ' ' new.txt new2.txt > new3.txt
+
+# Result 1
 awk '{print $1 - $2}' new3.txt | sed 's/-//g' | awk '{sum += $1} END {print sum}'
-cat new.txt | xargs -I {} sh -c 'echo {} 0' | awk '{print  * }' | awk '{sum += } END {print sum}'
+
+# Result 2
+cat new.txt | xargs -I {} sh -c 'echo "{} $(grep -cF "{}" new2.txt)"' | awk '{print $1 * $2}' | awk '{sum += $1} END {print sum}'
+
+# Cleanup
+rm -rf new*.txt
